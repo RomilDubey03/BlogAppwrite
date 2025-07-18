a9d25ae345d1b3bf07ab564fa063266f69183329
Romil Dubey <romildubey03@gmail.com>
2025-07-18 16:06:31 +0530

    Add all files

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div className="text-4xl font-extrabold tracking-tight text-gray-900" >BlogItOut</div>
+  )
+}
+
+export default Logo
